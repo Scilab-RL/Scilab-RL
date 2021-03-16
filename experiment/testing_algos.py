5159ea731b2bf8080bf64fbcdd7e22f2c73019a4
eppe <eppe@informatik.uni-hamburg.de>
2021-03-16 22:13:39 +0100

    Fixed bug with action scaling that occurred when action space is 0 in one dimension

--- experiment/testing_algos.py
+++ experiment/testing_algos.py
@@ -56,13 +56,13 @@ class TestingAlgos:
             performance_params = {'n_epochs': 600, 'n_runs': 6, 'min_success_runs': 4,
                                   'min_performance_np.abs(self.action_space.high - value': 0.9, 'performance_measure': 'test/success_rate'}
         elif env in ['FetchSlide-v1']:
-            performance_params = {'n_epochs': 300, 'n_runs': 6, 'min_success_runs': 4,
+            performance_params = {'n_epochs': 600, 'n_runs': 6, 'min_success_runs': 4,
                                   'min_performance_value': 0.9, 'performance_measure': 'test/success_rate'}
         elif env in ['FetchPickAndPlace-v1']:
-            performance_params = {'n_epochs': 300, 'n_runs': 6, 'min_success_runs': 4,
+            performance_params = {'n_epochs': 600, 'n_runs': 6, 'min_success_runs': 4,
                                   'min_performance_value': 0.9, 'performance_measure': 'test/success_rate'}
         elif env in ['HandReach-v0']:
-            performance_params = {'n_epochs': 300, 'n_runs': 6, 'min_success_runs': 4,
+            performance_params = {'n_epochs': 600, 'n_runs': 6, 'min_success_runs': 4,
                                   'min_performance_value': 0.9, 'performance_measure': 'test/success_rate'}
         else:
             print("Environment {} is not evaluated with HER algorithm.".format(env))
