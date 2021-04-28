4366b424fa28ebce3a91ec390ae2eb17c0efdb4f
eppe <eppe@informatik.uni-hamburg.de>
2021-04-28 17:05:15 +0200

    hyperopt seems to run, although not fully meeting all desired features mentioned in the git issue.

--- experiment/hyperopt.py
+++ experiment/hyperopt.py
@@ -1,6 +1,6 @@
 import matplotlib
-# matplotlib.use("TkAgg")
-from matplotlib import  pyplot as plt
+matplotlib.use("pdf")
+from matplotlib import pyplot as plt
 # DB_NAME = 'ideas_hrl_hyperopt_database'
 # DB_USER = 'hyperopt_user'
 # DB_PW = 'Ideas21!'
@@ -33,12 +33,13 @@ from optuna.visualization import plot_optimization_history
 from optuna.visualization import plot_parallel_coordinate
 from optuna.visualization import plot_param_importances
 from optuna.visualization import plot_slice
+from matplotlib.backends.backend_pdf import PdfPages
 
 
 PROCS_RUNNING = 0
 MINUTES_TO_RUN = 0.5
 HOURS_TO_RUN = MINUTES_TO_RUN / 60
-RUNS_PER_PARAM = 3
+MAX_RUNS_PER_PARAM = 3
 
 
 def log_params_from_omegaconf_dict(params):
@@ -105,8 +106,8 @@ def check_resources_free(free_ram=1000, free_gpu_ram=2000, free_cpus=2):
 
     return cpus_free and gpu_free and ram_free
 
-def do_train(cfg: DictConfig) -> float:
-
+def do_train(cfg: DictConfig, queue=None) -> float:
+    np.random.seed()
     data_path = hydra.utils.get_original_cwd()
     dataset = MNIST(data_path, download=True, transform=transforms.ToTensor())
     train, val = random_split(dataset, [55000, 5000])
@@ -153,8 +154,6 @@ def do_train(cfg: DictConfig) -> float:
             test_acc = 0
             with torch.set_grad_enabled(False):
                 for i, (x, y) in enumerate(testloader):
-                    # steps = epoch * len(trainloader) + i
-                    # optimizer.zero_grad()
 
                     outputs = model(x)
                     test_loss += criterion(outputs, y)
@@ -170,50 +169,59 @@ def do_train(cfg: DictConfig) -> float:
                 mlflow.log_metric("test_acc", float(test_acc), step=steps)
                 mlflow.log_metric("test_loss", float(test_loss), step=steps)
                 print(f"Test accuracy of epoch {epoch}: {test_acc}")
+
+    if queue is not None:
+        queue.put(test_acc)
     return test_acc
 
 def check_cfg_duplicate(cfg, metric='', max_duplicates=1):
     experiment = mlflow.get_experiment_by_name(cfg.mlflow.runname)
-
     try:
         runs = mlflow.list_run_infos(experiment.experiment_id)
     except:
         runs = []
-    n_duplicates = 0
+    n_duplicate_vals = 0
     duplicate_val = 0
+    # n_duplicate_runs = 0
     for r in runs:
         r_info = mlflow.get_run(r.run_id)
         cfg_duplicate = True
         for k,v in r_info.data.params.items():
             k_items = k.split(".")[1:]
             cfg_item = cfg
+            invalid_param = False
             for k_item in k_items:
-                cfg_item = cfg_item[k_item]
+                if k_item in cfg_item.keys():
+                    cfg_item = cfg_item[k_item]
+                else:
+                    invalid_param = True
+            if invalid_param:
+                continue
             this_run_v = str(cfg_item)
             if this_run_v != v:
                 cfg_duplicate = False
                 break
         if cfg_duplicate is True:
-            n_duplicates += 1
-            if metric != '':
+            # n_duplicate_runs += 1
+            if metric != '' and metric in r_info.data.metrics.keys():
                 metric_val = r_info.data.metrics[metric]
                 duplicate_val += metric_val
-        if n_duplicates > max_duplicates: # One duplicate is fine because the current run is already registered. But no more than one.
+                n_duplicate_vals += 1
+
+        if n_duplicate_vals > max_duplicates: # One duplicate is fine because the current run is already registered. But no more than one.
             break
-    is_duplicate = n_duplicates >= max_duplicates
+    is_duplicate = n_duplicate_vals >= max_duplicates
     if metric != '':
-        if n_duplicates > 0:
-            avg_metric_val = duplicate_val / n_duplicates
+        if n_duplicate_vals > 0:
+            avg_metric_val = duplicate_val / n_duplicate_vals
         else:
             avg_metric_val = None
+            # is_duplicate = False # In this case there are duplicate runs but these are not yet finished so we cannot get their value. If this is the case we just claim that this is not a duplicate and evaluate the run normally.
         return is_duplicate, avg_metric_val
     else:
         return is_duplicate
 
-def my_func(cfg: DictConfig, queue) -> float:
-    # mlflow.set_tracking_uri('file://' + hydra.utils.get_original_cwd() + '/mlruns')
-    # tracking_uri = mlflow.get_tracking_uri()
-    # print("Current tracking uri: {}".format(tracking_uri))
+def my_func(cfg: DictConfig, queue=None) -> float:
     np.random.seed()
     print(f"current proc: {mp.current_process()}")
     print(f"fc1: {cfg.model.fc1_units}")
@@ -223,116 +231,116 @@ def my_func(cfg: DictConfig, queue) -> float:
         rndn = np.random.normal()
         t_sleep = quotient * 4 + quotient * rndn
         t_sleep /= 10
-        # t_sleep = 6
         print(f"sleeping for {t_sleep} secs.")
-        n_loops = int(t_sleep * 10000)
+        n_loops = int(t_sleep * 100000)
         a = 1
         for _ in range(n_loops):
             a *= 1.000001
         # time.sleep(t_sleep) # time.sleep triggers a KeyboardInterrupt when running in multiprocess mode
-        # start = time.process_time()
-        # while (start + t_sleep) > time.process_time():
-        #   var_a = 1
         mlflow.log_param("cfg.model.fc1_units", cfg.model.fc1_units)
         mlflow.log_param("cfg.model.fc2_units", cfg.model.fc2_units)
         mlflow.log_metric("t_sleep", float(t_sleep))
         mlflow.log_metric("hyperopt_score", float(t_sleep))
-    queue.put(t_sleep)
+    if queue is not None:
+        queue.put(t_sleep)
     return t_sleep
 
-def proc_finished_cb(result):
-    global PROCS_RUNNING, RUNS_PER_PARAM
+def proc_finished_cb(result=None):
+    global PROCS_RUNNING, MAX_RUNS_PER_PARAM
     print(f"Proc done with result {result}")
-    PROCS_RUNNING -= RUNS_PER_PARAM
+    PROCS_RUNNING -= MAX_RUNS_PER_PARAM
 
 @hydra.main(config_name='config.yaml')
-def main(cfg: DictConfig) -> float:
-    global RUNS_PER_PARAM
+def main(cfg: DictConfig, *args) -> float:
+    global MAX_RUNS_PER_PARAM, PROCS_RUNNING
     mlflow.set_tracking_uri('file://' + hydra.utils.get_original_cwd() + '/mlruns')
     tracking_uri = mlflow.get_tracking_uri()
     print("Current tracking uri: {}".format(tracking_uri))
     mlflow.set_experiment(cfg.mlflow.runname)
     # time.sleep(1)
-    is_duplicate, duplicate_score = check_cfg_duplicate(cfg, metric='hyperopt_score', max_duplicates=RUNS_PER_PARAM)
+    is_duplicate, duplicate_score = check_cfg_duplicate(cfg, metric='hyperopt_score', max_duplicates=MAX_RUNS_PER_PARAM)
     if is_duplicate:
         print("This parameterization has been tried before, will just return the results from last time.")
         return duplicate_score
 
-        # inner_pool.apply_async(my_func, cfg, callback=proc_finished_cb)
+    PROCS_RUNNING += MAX_RUNS_PER_PARAM
 
-    ### Parallel runs with pool. # NOT WORKING
-    # inner_pool = mp.Pool(processes=RUNS_PER_PARAM)
-    # for _ in range(RUNS_PER_PARAM):
-    #     inner_pool.map(my_func, cfg)
+    func_to_train = do_train
+
+    ### Parallel runs with pool, sync and async version. # NOT WORKING
+    # inner_pool = mp.Pool(processes=MAX_RUNS_PER_PARAM)
+    # for _ in range(MAX_RUNS_PER_PARAM):
+    #     inner_pool.map(func_to_train, cfg) # sync execution
+    #     inner_pool.apply_async(func_to_train, cfg, callback=proc_finished_cb) # or async execution
     # # print(res)
     # result = 0
 
     ### Parallel runs with multiprocess.
-    value_queue = mp.Queue()
-    processes = [Process(target=my_func, args=(cfg,value_queue)) for x in range(RUNS_PER_PARAM)]
-    for p in processes:
-
-        p.start()
-
-    result = 0
-    for p in processes:
-        p.join()
-    while not value_queue.empty():
-        result += value_queue.get()
-    result /= RUNS_PER_PARAM
+    # value_queue = mp.Queue()
+    # processes = [Process(target=func_to_train, args=(cfg,value_queue)) for x in range(MAX_RUNS_PER_PARAM)]
+    # for p in processes:
+    #     p.start()
+    #
+    # result = 0
+    # for p in processes:
+    #     p.join()
+    # while not value_queue.empty():
+    #     result += value_queue.get()
+    # result /= MAX_RUNS_PER_PARAM
+    # proc_finished_cb()
 
     ### SINGLE RUN:
-    # result = my_func(cfg)
+    result = func_to_train(cfg)
+    proc_finished_cb()
 
     ### AVG over consecutive runs:
     # result = 0
-    # for _ in range(RUNS_PER_PARAM):
-    #     res = my_func(cfg)
+    # for _ in range(MAX_RUNS_PER_PARAM):
+    #     res = func_to_train(cfg)
     #     result += res
-    #
-    # result /= RUNS_PER_PARAM
+    # result /= MAX_RUNS_PER_PARAM
+    # proc_finished_cb()
 
     return result
 
 if __name__ == "__main__":
-    # main()
-    # mlflow.set_tracking_uri('file://' + hydra.utils.get_original_cwd() + '/mlruns')
-    # tracking_uri = mlflow.get_tracking_uri()
-    # print("Current tracking uri: {}".format(tracking_uri))
-    # mlflow.set_experiment(cfg.mlflow.runname)
-
-    n_cpus = psutil.cpu_count(logical=True) - 1
-
-    # prune_params = ParamRepeatPruner(study)
-    # runs_per_param = 3
-    # print(f"We will use {n_cpus} cpus")
-    # # n_cpus = mp.cpu_count()-1 # leave one cpu free for other tasks the machine has to do.
-    n_outer_pool_procs = n_cpus // RUNS_PER_PARAM
-    # n_outer_pool_procs = 1
-    outer_pool = mp.Pool(n_outer_pool_procs)
+    # score = main()
+    # n_cpus = psutil.cpu_count(logical=True) - 1
     #
-    sec_to_run = HOURS_TO_RUN * 60 * 60
-    start_time = time.time()
-    while True:
-        runtime = time.time() - start_time
-        runtime_hours = runtime / 3600
-        if runtime >= sec_to_run:
-            break
-        while True:
-            res_free = check_resources_free(free_cpus=RUNS_PER_PARAM + 1)
-            if res_free:
-                break
-            time.sleep(5)
-        print(f"Starting next processes. Running for {runtime_hours:.2f} of {HOURS_TO_RUN:.2f} hours now.")
-        # outer_pool.apply(main)
-        main()
+    # # n_outer_pool_procs = n_cpus // MAX_RUNS_PER_PARAM
+    # n_outer_pool_procs = 1
+    # outer_pool = mp.Pool(n_outer_pool_procs)
+    #
+    main()
+
+    # sec_to_run = HOURS_TO_RUN * 60 * 60
+    # start_time = time.time()
+    # while True:
+    #     runtime = time.time() - start_time
+    #     runtime_hours = runtime / 3600
+    #     if runtime >= sec_to_run:
+    #         break
+    #     # while True:
+    #     #     res_free = check_resources_free(free_cpus=MAX_RUNS_PER_PARAM + 1)
+    #     #     if res_free:
+    #     #         break
+    #     #     else:
+    #     #         print("Waiting for free resources")
+    #     # time.sleep(5)
+    #     print(f"Starting next processes. Running for {runtime_hours:.2f} of {HOURS_TO_RUN:.2f} hours now.")
+    #     # outer_pool.apply_async(main, callback=proc_finished_cb)
+    #     # outer_pool.apply(main, range(n_outer_pool_procs))
+    #     main()
 
     study = optuna.load_study("simple_mnist", "sqlite:///simple_mnist_hyperopt.db")
-    plot_optimization_history(study)
-    plt.show()
-    # plot_intermediate_values(study)
-    plot_contour(study)
-    #     # outer_pool.apply_async(main, callback=proc_finished_cb)
-    #     PROCS_RUNNING += runs_per_param
-
-    plot_param_importances(study)
+    # with PdfPages('hyperopt_pdf.pdf') as pdf:
+    if not os.path.exists("hyperopt_logs"):
+        os.mkdir("hyperopt_logs")
+    fig = plot_optimization_history(study)
+    fig.write_image("hyperopt_logs/plot_optimization_history.png")
+    fig = plot_contour(study)
+    fig.write_image("hyperopt_logs/plot_contour.png")
+    fig = plot_param_importances(study)
+    fig.write_image("hyperopt_logs/plot_param_importances.png")
+    fig = plot_intermediate_values(study)
+    fig.write_image("hyperopt_logs/plot_intermediate_values.png")
