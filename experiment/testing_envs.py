b7045a525d5e6c4b91279276a14fd1027a48300e
eppe <eppe@informatik.uni-hamburg.de>
2021-04-29 12:56:13 +0200

    Ant Fall not yet working, but everything else.

--- experiment/testing_envs.py
+++ experiment/testing_envs.py
@@ -1,59 +1,31 @@
 # environments used for testing
 class TestingEnvs:
     env_names = [
-        # 'CartPole-v1',
-        # 'MountainCarContinuous-v0',
-        # 'HandReach-v0',
+        'CartPole-v1',
+        'MountainCarContinuous-v0',
+        'HandReach-v0',
 
-        # 'FetchPush-v1',
-        # 'FetchSlide-v1',
-        # 'FetchPickAndPlace-v1',
-        # 'FetchReach-v1',
+        'FetchPush-v1',
+        'FetchSlide-v1',
+        'FetchPickAndPlace-v1',
+        'FetchReach-v1',
 
-        # 'HandManipulateBlock-v0',
-        # 'Hook-o1-v1',
-        # 'ButtonUnlock-o2-v1',
+        'HandManipulateBlock-v0',
+        'Hook-o1-v1',
+        'ButtonUnlock-o2-v1',
         'ButtonUnlock-o1-v1',
 
+        'AntButtonUnlock-o1-v1',
         'AntReacher-v1',
-        # 'Ant4Rooms-v1',
+        'Ant4Rooms-v1',
         'AntMaze-v0',
         'AntPush-v0',
         'AntFall-v0',
 
-        # 'BlockStackMujocoEnv-gripper_random-o0-v1',
-        # 'BlockStackMujocoEnv-gripper_random-o2-v1',
-        # 'BlockStackMujocoEnv-gripper_above-o1-v1',
-        # 'BlockStackMujocoEnv-gripper_none-o1-v1',
+        'BlockStackMujocoEnv-gripper_random-o0-v1',
+        'BlockStackMujocoEnv-gripper_random-o2-v1',
+        'BlockStackMujocoEnv-gripper_above-o1-v1',
+        'BlockStackMujocoEnv-gripper_none-o1-v1',
     ]
 
-    # def get_CartPole_v1_cmds(self):
-    #     cmd = " --env CartPole-v1"
-    #     return [cmd]
 
-# env_names = [
-#     'AntReacherEnv-v0',
-#     'AntFourRoomsEnv-v0',
-#     'AntMazeEnv-v0',
-#     'AntPushEnv-v0',
-#     'AntFallEnv-v0',
-#
-#     'AntCausalDepEnv-o0-v0',
-#     'AntCausalDepEnv-o1-v0',
-#     'AntCausalDepEnv-o2-v0',
-#
-#     'BlockStackMujocoEnv-gripper_random-o0-v1',
-#     'BlockStackMujocoEnv-gripper_random-o2-v1',
-#     'BlockStackMujocoEnv-gripper_above-o1-v1',
-#     'BlockStackMujocoEnv-gripper_none-o1-v1',
-#
-#     'HookMujocoEnv-gripper_above-e1-v1',
-#
-#     'CausalDependenciesMujocoEnv-o0-v0',
-#     'CausalDependenciesMujocoEnv-o1-v0',
-#     'CausalDependenciesMujocoEnv-o2-v0',
-#
-#     'CopReacherEnv-ik1-v0',
-#     'CopReacherEnv-ik0-v0',
-#     'UR5ReacherEnv-v1',
-# ]
