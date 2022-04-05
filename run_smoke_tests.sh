fd72555005a25193ee04266751a7a2040d6335f5
6fryen <6fryen@informatik.uni-hamburg.de>
2022-04-05 13:24:39 +0200

    split smoke testing into env and algorithm testing to shorten runtime

--- run_smoke_tests.sh
+++ run_smoke_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 main() {
+  # test selection of environments with sac
   local ALGS="sac"
 
   local ENVS=""
@@ -29,6 +30,13 @@ main() {
   ENVS+="close_box-state-v0"
 
   python3 experiment/train.py algorithm=$ALGS env=$ENVS ++n_epochs=1 +defaults=smoke_test --multirun;
+
+  # test other Algorithms
+  local ALG_TEST_ENVS="FetchReach-v1,AntReacher-v1,reach_target-state-v0"
+  # test HER
+  python3 experiment/train.py algorithm=sac env=$ALG_TEST_ENVS +replay_buffer=her ++n_epochs=1 +defaults=smoke_test --multirun;
+  # test ddpg, td3 and hac
+  python3 experiment/train.py algorithm=ddpg,td3,hac env=$ALG_TEST_ENVS ++n_epochs=1 +defaults=smoke_test --multirun;
 }
 
 main;
