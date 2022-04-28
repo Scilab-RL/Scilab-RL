4af1d23f0b787792c0ec88266356f11a99704dd6
Thilo Fryen <thilo.fryen@uni-hamburg.de>
2022-04-28 19:00:50 +0200

    shortened smoke tests

--- run_smoke_tests.sh
+++ run_smoke_tests.sh
@@ -15,17 +15,11 @@ main() {
   ENVS+="Ant4Rooms-v1,"
   ENVS+="AntButtonUnlock-o2-v1,"
   ENVS+="AntMaze-v0,"
-  ENVS+="AntPush-v0,"
-  ENVS+="AntFall-v0,"
   ENVS+="Blocks-o0-gripper_random-v1,"
-  ENVS+="Blocks-o1-gripper_above-v1,"
   ENVS+="Blocks-o3-gripper_none-v1,"
   #RLBench
   ENVS+="reach_target-state-v0,"
-  ENVS+="turn_tap-state-v0,"
-  ENVS+="close_laptop_lid-state-v0,"
   ENVS+="close_drawer-state-v0,"
-  ENVS+="close_box-state-v0"
 
   FAILED_RUNS=0
 
