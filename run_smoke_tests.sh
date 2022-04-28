75333bea0a693f29779d4e4a7ea2271a8bfa5583
Thilo Fryen <thilo.fryen@uni-hamburg.de>
2022-04-28 18:55:25 +0200

    refactor smoke tests to run without display, added exit code

--- run_smoke_tests.sh
+++ run_smoke_tests.sh
@@ -7,8 +7,6 @@ main() {
   local ENVS=""
   #MuJoCo
   ENVS+="FetchReach-v1,"
-  ENVS+="FetchPush-v1,"
-  ENVS+="FetchSlide-v1,"
   ENVS+="FetchPickAndPlace-v1,"
   ENVS+="HandManipulateBlock-v0,"
   ENVS+="Hook-o1-v1,"
@@ -29,14 +27,27 @@ main() {
   ENVS+="close_drawer-state-v0,"
   ENVS+="close_box-state-v0"
 
-  python3 experiment/train.py algorithm=$ALGS env=$ENVS ++n_epochs=1 ++wandb=0 +defaults=smoke_test --multirun;
+  FAILED_RUNS=0
+
+  # Don't have xvfb? install it with sudo apt-get install xvfb
+  xvfb-run -a python3 experiment/train.py algorithm=$ALGS env=$ENVS ++n_epochs=1 ++wandb=0 +defaults=smoke_test --multirun;
+  (( FAILED_RUNS+= $? ))
 
   # test other Algorithms
   local ALG_TEST_ENVS="FetchReach-v1,AntReacher-v1,reach_target-state-v0"
   # test HER
-  python3 experiment/train.py algorithm=sac env=$ALG_TEST_ENVS +replay_buffer=her ++n_epochs=1 ++wandb=0 +defaults=smoke_test --multirun;
+  xvfb-run -a python3 experiment/train.py algorithm=sac env=$ALG_TEST_ENVS +replay_buffer=her ++n_epochs=1 ++wandb=0 +defaults=smoke_test --multirun;
+  (( FAILED_RUNS+= $? ))
+
   # test ddpg, td3 and sacvg
-  python3 experiment/train.py algorithm=ddpg,td3,sacvg env=$ALG_TEST_ENVS ++n_epochs=1 ++wandb=0 +defaults=smoke_test --multirun;
+  xvfb-run -a python3 experiment/train.py algorithm=ddpg,td3,sacvg env=$ALG_TEST_ENVS ++n_epochs=1 ++wandb=0 +defaults=smoke_test --multirun;
+  (( FAILED_RUNS+= $? ))
 }
 
-main;
+main
+if (($FAILED_RUNS < 1)); then
+  printf "All smoke tests passed successfully."
+  exit 0
+fi
+printf "At least one smoke test failed."
+exit 1
