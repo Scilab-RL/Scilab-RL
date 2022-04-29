effc0659f9ad89b5c198bcee2d1663a624c2d849
Thilo Fryen <thilo.fryen@uni-hamburg.de>
2022-04-29 12:03:40 +0200

    shorten s-&p-tests so pipeline runs in <1h

--- run_smoke_tests.sh
+++ run_smoke_tests.sh
@@ -6,19 +6,19 @@ main() {
 
   local ENVS=""
   #MuJoCo
-  ENVS+="FetchReach-v1,"
+  # ENVS+="FetchReach-v1,"
   ENVS+="FetchPickAndPlace-v1,"
   ENVS+="HandManipulateBlock-v0,"
   ENVS+="Hook-o1-v1,"
   ENVS+="ButtonUnlock-o2-v1,"
   ENVS+="AntReacher-v1,"
-  ENVS+="Ant4Rooms-v1,"
+  # ENVS+="Ant4Rooms-v1,"
   ENVS+="AntButtonUnlock-o2-v1,"
   ENVS+="AntMaze-v0,"
   ENVS+="Blocks-o0-gripper_random-v1,"
   ENVS+="Blocks-o3-gripper_none-v1,"
   #RLBench
-  ENVS+="reach_target-state-v0,"
+  # ENVS+="reach_target-state-v0,"
   ENVS+="close_drawer-state-v0"
 
   FAILED_RUNS=0
