1eba07c889f63b8d5adffdd3cb92be26aa8be91a
Frank Roeder <frank.roeder@tuhh.de>
2022-05-05 11:37:59 +0200

    Adjust performance test script

--- run_performance_tests.sh
+++ run_performance_tests.sh
@@ -2,12 +2,13 @@ RUN_ALL_TESTS="false" # set to "true" to run all tests
 
 if [ $RUN_ALL_TESTS != "true" ]
 then  # run only these performance tests:
-  for config in "FetchReach/sac_her-test" # "RLB_reach_target/sac_her-test"
+  for config in "FetchReach/sac_her-test" "FetchPush/sac_her-test" "FetchSlide/sac_her-test" "FetchPickAndPlace/sac_her-test" "Blocks/o0-random-sac_her-test" "Blocks/o1-random-sac_her-test" "Blocks/o1-above-sac_her-test" "Blocks/o1-none-sac_her-test" "Ant4Rooms/sac_her-test" "AntReacher/sac_her-test"
   do
-    if ! xvfb-run -a python3 experiment/train.py +performance=$config wandb=0 --multirun;
-    then
-      exit 1
-    fi
+    # echo "python3 experiment/train.py +performance=$config wandb=1 project_name=perf_test_$config --multirun;"
+		if ! xvfb-run -a python3 experiment/train.py +performance=$config wandb=0 --multirun;
+		then
+			exit 1
+		fi
   done
   exit 0
 fi
