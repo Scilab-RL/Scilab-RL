a395c8390960efbc447a344d58bf39dfdc1b637e
Thilo Fryen <thilo.fryen@uni-hamburg.de>
2022-05-06 12:40:31 +0200

    started with basic custom algo

---
+++ custom_algorithms/basic/basic.py
@@ -0,0 +1,16 @@
+"""
+This is the most basic algorithm that works with our framework.
+"""
+
+import numpy as np
+
+
+class BASIC:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def load(self):
+        pass
