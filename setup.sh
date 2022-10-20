7e594602f91ab39cf20db1e31363c6947f36bc6f
Jan Dohmen <jan.dohmen@tuhh.de>
2022-10-20 11:17:04 +0200

    Fix typo

--- setup.sh
+++ setup.sh
@@ -147,7 +147,7 @@ main() {
   success "RLBench installed/updated"
   success "Installation complete."
   info "You must now run source ~/.bashrc to activate conda. Alternatively, you can just restart this shell"
-  info "Additonally execute bash set_paths.sh or copy the relevant environment variables into your .bashrc or IDE."
+  info "Additionally execute bash set_paths.sh or copy the relevant environment variables into your .bashrc or IDE."
 }
 
 main "$@"
