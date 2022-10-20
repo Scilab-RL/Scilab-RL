9df5b54d7c64c73d13505e3af75a85c7acb6fa99
Jan Dohmen <jan.dohmen@tuhh.de>
2022-10-20 15:08:31 +0200

    Export nvidia related paths only of nvidia gpu available

--- set_paths.sh
+++ set_paths.sh
@@ -8,9 +8,11 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
 
 #For both
-nv_version_long=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
-nv_version=${nv_version_long:0:3}
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-$nv_version
-#export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libGLEW.so # Had to comment this out with NVIDIA driver 460 version.
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-000
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+if [ -x "$(command -v nvidia-smi)" ]; then
+  nv_version_long=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
+  nv_version=${nv_version_long:0:3}
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-$nv_version
+  #export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libGLEW.so # Had to comment this out with NVIDIA driver 460 version.
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia-000
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+fi
