272eacff7282632b804d8ea1a32dfc682131e5ad
Frank Roeder <frank.roeder@tuhh.de>
2022-11-03 16:42:27 +0100

    Remove `learning_starts` limitations

--- util/util.py
+++ util/util.py
@@ -71,8 +71,4 @@ def avoid_start_learn_before_first_episode_finishes(alg_kwargs, env):
             "use a `gym.wrappers.TimeLimit` wrapper "
             "or pass `max_episode_length` to the model constructor"
         )
-    if 'learning_starts' in alg_kwargs:
-        alg_kwargs['learning_starts'] = max(alg_kwargs['learning_starts'], max_ep_steps)
-    else:
-        alg_kwargs['learning_starts'] = max_ep_steps
     return alg_kwargs
