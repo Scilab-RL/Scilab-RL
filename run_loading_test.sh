8a41072ab850249cf54047e65ff51f2348e26641
Frank Roeder <frank.roeder@tuhh.de>
2023-01-11 12:24:58 +0100

    Add dedicated scripts to test rendering and restoring policy

---
+++ run_loading_test.sh
@@ -0,0 +1,6 @@
+test_loading() {
+  # restore an agent from the previous smoke test
+  SMOKE_TEST_DIR=/builds/$USER/Scilab-RL/data/2327a47/FetchPickAndPlace-v1/11-15-15/0/rl_model_best.zip
+  python main.py env=FetchReach-v1 algorithm=sac +restore_policy=$SMOKE_TEST_DIR render=none wandb=0 n_epochs=1 eval_after_n_steps=1
+}
+echo "Passed loading test"
