7daa7a6e21d14620cda5cd664bfcd955e7e91d39
Frank Roeder <frank.roeder@tuhh.de>
2023-01-11 12:51:28 +0100

    Fix smoke test
    - remove render test

--- run_smoke_tests.sh
+++ run_smoke_tests.sh
@@ -52,23 +52,7 @@ test_envs() {
     exit 1
   fi
 }
-test_render() {
-  # test render on different types of environments
-  local ENVS=""
-  # MuJoCo
-  ENVS+="FetchPickAndPlace-v1,"
-  # CoppeliaSim
-  ENVS+="close_box-state-v0,"
-  # Box2D physics engine
-  ENVS+="parking-limited-v0"
-  if ! xvfb-run -a python3 main.py algorithm=sac env=$ENVS +performance=smoke_test render="record" render_freq=1 --multirun;
-  then
-    exit 1
-  fi
-
-}
 export CUDA_VISIBLE_DEVICES=""
 test_algos
 test_envs
-test_render
 echo "All smoke tests passed successfully."
