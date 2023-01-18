4fa0b7bda6c4aabcfd522f7b35b37cdbdb714612
Frank Roeder <frank.roeder@tuhh.de>
2023-01-18 10:47:51 +0100

    Move render and restore tests into smoke test
    - adjust gitlab-ci

--- run_loading_tests.sh
+++
@@ -1,9 +0,0 @@
-test_loading() {
-  # restore an agent from the previous smoke test
-  SMOKE_TEST_DIR=/builds/$USER/Scilab-RL/data/2327a47/FetchPickAndPlace-v1/11-15-15/0/rl_model_best.zip
-  if ! xvfb-run -a python3 main.py env=FetchReach-v1 algorithm=sac +restore_policy=$SMOKE_TEST_DIR render=none wandb=0
-  then
-    exit 1
-  fi
-}
-echo "Passed loading test"
