3be9ecda8833e31e871c51e477b0f3a80731d509
Frank Roeder <frank.roeder@tuhh.de>
2023-02-07 14:43:28 +0100

    Go back to pickle

--- custom_algorithms/basic/basic.py
+++ custom_algorithms/basic/basic.py
@@ -1,5 +1,6 @@
 import torch as th
 import numpy as np
+import pickle
 
 # mean absolute error
 mae = th.nn.L1Loss(reduction='sum')
@@ -123,7 +124,7 @@ class BASIC:
     @classmethod
     def load(cls, path, env, **kwargs):
         model = cls(env=env, **kwargs)
-        loaded_dict = th.load(open(path, "rb"))
+        loaded_dict = pickle.load(open(path, "rb"))
         for k in loaded_dict:
             if k not in ["actor_state", "critic_state"]:
                 model.__dict__[k] = loaded_dict[k]
@@ -142,7 +143,7 @@ class BASIC:
         data["actor_state"] = self.actor.state_dict()
         data["critic_state"] = self.critic.state_dict()
         # no need to save the target-network state, because it is a copy of the critic network
-        th.save(data, open(path + ".zip", "wb"))
+        pickle.dump(data, open(path + ".zip", "wb"))
 
     # deterministic is true when the policy is being evaluated
     # this is requred for viz during eval
