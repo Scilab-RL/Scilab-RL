490d6d86d4c060aaac2fe70b363f9a7f330f7979
ThiFry <thilo.fryen@uni-hamburg.de>
2023-09-15 13:56:18 +0200

    changes for new hydra and optuna

--- src/hydra_plugins/hydra_custom_optuna_sweeper/performance_testing_sweeper.py
+++ src/hydra_plugins/hydra_custom_optuna_sweeper/performance_testing_sweeper.py
@@ -2,10 +2,9 @@ import datetime
 import time
 import logging
 from typing import List
-from hydra.core.config_loader import ConfigLoader
 from hydra.core.plugins import Plugins
 from hydra.plugins.sweeper import Sweeper
-from hydra.types import TaskFunction
+from hydra.types import TaskFunction, HydraContext
 from omegaconf import DictConfig
 from mlflow.tracking import MlflowClient
 
@@ -35,14 +34,14 @@ class PerformanceTestingSweeper(Sweeper):
     def setup(
         self,
         config: DictConfig,
-        config_loader: ConfigLoader,
+        hydra_context: HydraContext,
         task_function: TaskFunction,
     ) -> None:
         self.config = config
-        self.config_loader = config_loader
+        self.hydra_context = hydra_context
         self.task_function = task_function
         self.launcher = Plugins.instance().instantiate_launcher(
-            config=config, config_loader=config_loader, task_function=task_function)
+            config=config, hydra_context=hydra_context, task_function=task_function)
 
     def sweep(self, arguments: List[str]) -> None:
         for arg in arguments:
