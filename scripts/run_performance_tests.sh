2d323b81b6100b9a8990aac89e44a3b16bdfff05
Manfred Eppe <manfred.eppe@tuhh.de>
2023-11-13 14:59:17 +0100

    working on performance testing script

--- scripts/run_performance_tests.sh
+++ scripts/run_performance_tests.sh
@@ -20,7 +20,8 @@ fi
 configs_to_ignore=("o0-random-cleansac_her-test" "o0-random-sac_her-test")
 unsuccessful_configs=()
 git_branch=$(git rev-parse --abbrev-ref HEAD)
-echo "Performance test starting on $(date). Branch: $git_branch " > performance-test_results.log
+commit_hash=$(git rev-parse $git_branch)
+echo "Performance test starting on $(date).\n Branch: $git_branch. \n commit: $git_branch" > performance-test_results.log
 for env_folder in "conf/performance"/*
 do
   for config in "$env_folder"/*
