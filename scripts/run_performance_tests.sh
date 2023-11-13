5ab482d6ef66f6a15e4ac41c106ef2f36948264f
Manfred Eppe <manfred.eppe@tuhh.de>
2023-11-13 15:23:28 +0100

    working on performance testing script

--- scripts/run_performance_tests.sh
+++ scripts/run_performance_tests.sh
@@ -29,7 +29,8 @@ do
   do
 #    if [[ $(echo ${configs_to_ignore[@]} | fgrep -w $config) ]]
 #    if [[ " ${configs_to_ignore[*]} " =~ " ${config} " ]]
-    if printf '%s\0' "${configs_to_ignore[@]}" | grep $config
+#    if printf '%s\0' "${configs_to_ignore[@]}" | grep $config
+    if [[ " ${configs_to_ignore[*]} " =~ " ${config} " ]]
     then
       echo "Skipping config $config"
       echo "Skipping config $config" >> performance-test_results.log
