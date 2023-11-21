20b3a11bed4b34b69950d2bd3c4ab613b191e88e
Frank Roeder <fr.coding@proton.me>
2023-11-21 11:26:42 +0100

    Add logging of mc metrics

--- src/custom_algorithms/cleansacmc/cleansacmc.py
+++ src/custom_algorithms/cleansacmc/cleansacmc.py
@@ -244,6 +244,13 @@ class CLEANSACMC:
 
         # perform action
         new_obs, rewards, dones, infos = self.env.step(actions)
+        mc_obs = torch.from_numpy(self._last_obs)
+        if len(mc_obs.shape) == 1:
+            mc_obs.unsqueeze_(0)
+        fw_normal, wm_normal = self.mc_network(mc_obs, torch.from_numpy(actions))
+        kl_div = torch.distributions.kl_divergence(fw_normal, wm_normal)
+        self.logger.record("mc_mean", kl_div.mean().item(), exclude="tensorboard")
+        self.logger.record("mc_std", kl_div.std().item(), exclude="tensorboard")
 
         self.num_timesteps += self.env.num_envs
 
