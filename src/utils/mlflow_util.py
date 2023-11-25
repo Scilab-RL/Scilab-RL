ad07e9f5535dd69ebbc8b4ae1392b1ed6a26bbf5
Manfred Eppe <manfred.eppe@tuhh.de>
2023-11-25 19:27:05 +0100

    Added possibility to hyperopt to optimize for data other than early_stop_threshold

--- src/utils/mlflow_util.py
+++ src/utils/mlflow_util.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def get_hyperopt_score(cfg, current_run):
-    data_key = cfg.early_stop_data_column
+    data_key = cfg.hyperopt_criterion
     client = mlflow.tracking.MlflowClient()
     hist = client.get_metric_history(current_run.info.run_id, data_key)
     data_val_hist = [h.value for h in hist]
@@ -14,8 +14,8 @@ def get_hyperopt_score(cfg, current_run):
     hyperopt_score = 0
     if epochs > 0:
         avg_data_val = np.mean(data_val_hist) / epochs
-
-        smooth_last_n = min(len(data_val_hist), cfg.early_stop_last_n)
+        # smooth_last_n values is 20% of data_val_hist
+        smooth_last_n = max(1, len(data_val_hist)//5)
         last_n_avg_val = np.mean(data_val_hist[-smooth_last_n:])
         smoothed_data_val_growth = last_n_avg_val / epochs
 
