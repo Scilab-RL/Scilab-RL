32a8072d588502b66b1470c5fda38091aebef52a
Frank Roeder <fr.coding@proton.me>
2023-12-01 17:27:20 +0100

    [cleansacmc] tiny adjustments
    - formatting
    - update distribution learning

--- src/custom_algorithms/cleansacmc/cleansacmc.py
+++ src/custom_algorithms/cleansacmc/cleansacmc.py
@@ -11,7 +11,7 @@ from gymnasium import spaces
 from stable_baselines3.common.logger import Logger
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback
-from stable_baselines3.common.buffers import DictReplayBuffer, ReplayBuffer
+from stable_baselines3.common.buffers import ReplayBuffer
 from stable_baselines3.her.her_replay_buffer import HerReplayBuffer
 from utils.custom_buffers import ErrorBuffer
 from .mc import MorphologicalNetworks
@@ -25,7 +25,9 @@ class Actor(nn.Module):
     def __init__(self, env, hidden_size):
         super().__init__()
         if isinstance(env.observation_space, spaces.dict.Dict):
-            obs_shape = np.sum([obs_space.shape for obs_space in env.observation_space.spaces.values()])
+            obs_shape = np.sum(
+                [obs_space.shape for obs_space in env.observation_space.spaces.values()]
+            )
         else:
             obs_shape = np.sum(env.observation_space.shape)
         self.fc1 = nn.Linear(obs_shape, hidden_size)
@@ -35,10 +37,18 @@ class Actor(nn.Module):
         self.fc_logstd = nn.Linear(hidden_size, np.prod(env.action_space.shape))
         # action rescaling
         self.register_buffer(
-            "action_scale", torch.tensor((env.action_space.high - env.action_space.low) / 2.0, dtype=torch.float32)
+            "action_scale",
+            torch.tensor(
+                (env.action_space.high - env.action_space.low) / 2.0,
+                dtype=torch.float32,
+            ),
         )
         self.register_buffer(
-            "action_bias", torch.tensor((env.action_space.high + env.action_space.low) / 2.0, dtype=torch.float32)
+            "action_bias",
+            torch.tensor(
+                (env.action_space.high + env.action_space.low) / 2.0,
+                dtype=torch.float32,
+            ),
         )
 
     def forward(self, x):
@@ -70,7 +80,9 @@ class Critic(nn.Module):
     def __init__(self, env, hidden_size):
         super().__init__()
         if isinstance(env.observation_space, spaces.dict.Dict):
-            obs_shape = np.sum([obs_space.shape for obs_space in env.observation_space.spaces.values()])
+            obs_shape = np.sum(
+                [obs_space.shape for obs_space in env.observation_space.spaces.values()]
+            )
         else:
             obs_shape = np.sum(env.observation_space.shape)
         self.fc1 = nn.Linear(obs_shape + np.prod(env.action_space.shape), hidden_size)
@@ -91,20 +103,19 @@ class CriticEnsemble(nn.Module):
     def __init__(self, env, n_critics: int, hidden_size):
         super().__init__()
         self._critics = nn.ModuleList(
-            [
-                Critic(env, hidden_size)
-                for _ in range(n_critics)
-            ]
+            [Critic(env, hidden_size) for _ in range(n_critics)]
         )
+
     def forward(self, x, a):
         return torch.stack([critic(x, a) for critic in self._critics])
+
 
 def flatten_obs(obs, device):
     if not isinstance(obs, dict):
         if isinstance(obs, np.ndarray):
             obs = torch.tensor(obs)
         return obs.to(device)
-    observation, ag, dg = obs['observation'], obs['achieved_goal'], obs['desired_goal']
+    observation, ag, dg = obs["observation"], obs["achieved_goal"], obs["desired_goal"]
     if isinstance(observation, np.ndarray):
         observation = torch.from_numpy(observation).to(device)
     if isinstance(ag, np.ndarray):
@@ -130,20 +141,21 @@ class CLEANSACMC:
         Set it to 'auto' to learn it automatically
     :param use_her: whether to use hindsight experience replay (HER) by using the SB3 HerReplayBuffer
     """
+
     def __init__(
-            self,
-            env: GymEnv,
-            learning_rate: float = 3e-4,
-            buffer_size: int = 1_000_000,
-            learning_starts: int = 100,
-            batch_size: int = 256,
-            tau: float = 0.005,
-            gamma: float = 0.99,
-            ent_coef: Union[str, float] = "auto",
-            use_her: bool = True,
-            n_critics: int = 2,
-            hidden_size: int = 256,
-            mc: dict = {}
+        self,
+        env: GymEnv,
+        learning_rate: float = 3e-4,
+        buffer_size: int = 1_000_000,
+        learning_starts: int = 100,
+        batch_size: int = 256,
+        tau: float = 0.005,
+        gamma: float = 0.99,
+        ent_coef: Union[str, float] = "auto",
+        use_her: bool = True,
+        n_critics: int = 2,
+        hidden_size: int = 256,
+        mc: dict = {},
     ):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.learning_rate = learning_rate
@@ -159,7 +171,9 @@ class CLEANSACMC:
         self.env = env
         if isinstance(self.env.action_space, spaces.Box):
             assert np.all(
-                np.isfinite(np.array([self.env.action_space.low, self.env.action_space.high]))
+                np.isfinite(
+                    np.array([self.env.action_space.low, self.env.action_space.high])
+                )
             ), "Continuous action space must have a finite lower and upper bound"
 
         # initialize replay buffer
@@ -170,7 +184,7 @@ class CLEANSACMC:
                 self.env.action_space,
                 env=self.env,
                 device=self.device,
-                n_envs=self.env.num_envs
+                n_envs=self.env.num_envs,
             )
         else:
             self.replay_buffer = ReplayBuffer(
@@ -178,18 +192,24 @@ class CLEANSACMC:
                 self.env.observation_space,
                 self.env.action_space,
                 device=self.device,
-                n_envs=self.env.num_envs
+                n_envs=self.env.num_envs,
             )
 
         self._create_actor_critic()
 
         self.ent_coef = ent_coef
         if self.ent_coef == "auto":
-            self.target_entropy = float(-np.prod(self.env.action_space.shape).astype(np.float32))
+            self.target_entropy = float(
+                -np.prod(self.env.action_space.shape).astype(np.float32)
+            )
             self.log_ent_coef = torch.zeros(1, device=self.device).requires_grad_(True)
-            self.ent_coef_optimizer = torch.optim.Adam([self.log_ent_coef], lr=self.learning_rate)
+            self.ent_coef_optimizer = torch.optim.Adam(
+                [self.log_ent_coef], lr=self.learning_rate
+            )
         else:
-            self.ent_coef_tensor = torch.tensor(float(self.ent_coef), device=self.device)
+            self.ent_coef_tensor = torch.tensor(
+                float(self.ent_coef), device=self.device
+            )
 
         self.logger = None
         self._last_obs = None
@@ -198,22 +218,31 @@ class CLEANSACMC:
 
     def _create_actor_critic(self) -> None:
         self.actor = Actor(self.env, self.hidden_size).to(self.device)
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.learning_rate)
-        self.critic = CriticEnsemble(self.env, self.n_critics, self.hidden_size).to(self.device)
-        self.critic_target = CriticEnsemble(self.env, self.n_critics, self.hidden_size).to(self.device)
+        self.actor_optimizer = torch.optim.Adam(
+            self.actor.parameters(), lr=self.learning_rate
+        )
+        self.critic = CriticEnsemble(self.env, self.n_critics, self.hidden_size).to(
+            self.device
+        )
+        self.critic_target = CriticEnsemble(
+            self.env, self.n_critics, self.hidden_size
+        ).to(self.device)
         self.critic_target.load_state_dict(self.critic.state_dict())
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.learning_rate)
+        self.critic_optimizer = torch.optim.Adam(
+            self.critic.parameters(), lr=self.learning_rate
+        )
 
         self.mc_network = MorphologicalNetworks(self.env, self.mc).to(self.device)
-        self.mc_optimizer = torch.optim.Adam(self.mc_network.parameters(), lr=self.mc["learning_rate"])
+        self.mc_optimizer = torch.optim.Adam(
+            self.mc_network.parameters(), lr=self.mc["learning_rate"]
+        )
         self.mc_err_buffer = ErrorBuffer(self.mc["err_buffer_size"], self.device)
 
-
     def learn(
-            self,
-            total_timesteps: int,
-            callback: MaybeCallback = None,
-            log_interval=None,
+        self,
+        total_timesteps: int,
+        callback: MaybeCallback = None,
+        log_interval=None,
     ):
         callback.init_callback(self)
         callback.on_training_start(locals(), globals())
@@ -237,10 +266,7 @@ class CLEANSACMC:
 
         return self
 
-    def collect_rollout(
-            self,
-            callback: BaseCallback
-    ):
+    def collect_rollout(self, callback: BaseCallback):
         """
         Collect experiences and store them into a ``ReplayBuffer``.
 
@@ -259,7 +285,9 @@ class CLEANSACMC:
         mc_obs = flatten_obs(self._last_obs, self.device)
         if len(mc_obs.shape) == 1:
             mc_obs.unsqueeze_(0)
-        fw_normal, wm_normal = self.mc_network(mc_obs.to(self.device), torch.from_numpy(actions).to(self.device))
+        fw_normal, wm_normal = self.mc_network(
+            mc_obs.to(self.device), torch.from_numpy(actions).to(self.device)
+        )
         kl_div = torch.distributions.kl_divergence(fw_normal, wm_normal)
         self.logger.record("mc_mean", kl_div.cpu().mean().item(), exclude="tensorboard")
         self.logger.record("mc_std", kl_div.cpu().std().item(), exclude="tensorboard")
@@ -271,7 +299,9 @@ class CLEANSACMC:
         for idx, done in enumerate(dones):
             if done:
                 real_next_obs[idx] = infos[idx]["terminal_observation"]
-        self.replay_buffer.add(self._last_obs, real_next_obs, actions, rewards, dones, infos)
+        self.replay_buffer.add(
+            self._last_obs, real_next_obs, actions, rewards, dones, infos
+        )
 
         self._last_obs = new_obs
 
@@ -282,9 +312,9 @@ class CLEANSACMC:
 
     def train_mc(self, observations, next_observations, actions):
         forward_normal, world_normal = self.mc_network(observations, actions)
-        fw_loss = -forward_normal.log_prob(next_observations).mean()
-        w_prime_a_loss = -world_normal.log_prob(next_observations).mean()
-        loss = fw_loss + w_prime_a_loss
+        fw_loss = -forward_normal.log_prob(next_observations)
+        w_prime_a_loss = -world_normal.log_prob(next_observations)
+        loss = (fw_loss + w_prime_a_loss).mean()
 
         self.logger.record("mc/fw_loss", fw_loss.item())
         self.logger.record("mc/w_prime_a_loss", w_prime_a_loss.item())
@@ -296,10 +326,10 @@ class CLEANSACMC:
     def calc_reward(self, observations, actions, e_rewards):
         forward_normal, world_normal = self.mc_network(observations, actions)
         _err = (
-                torch.distributions.kl.kl_divergence(forward_normal, world_normal)
-                .mean(-1)
-                .unsqueeze(1)
-            )
+            torch.distributions.kl.kl_divergence(forward_normal, world_normal)
+            .mean(-1)
+            .unsqueeze(1)
+        )
         if self.mc["minimize"]:
             _err = _err * -1.0
         if self.mc["reward_type"] in ["sparse", "scaled"]:
@@ -311,7 +341,7 @@ class CLEANSACMC:
                 i_rewards = ((_err - min_err) / (max_err - min_err)) - 1.0
             elif self.mc["reward_type"] == "scaled":
                 # reward in [0, 1]
-                i_rewards = ((_err - min_err) / (max_err - min_err))
+                i_rewards = (_err - min_err) / (max_err - min_err)
         else:
             i_rewards = _err
 
@@ -323,7 +353,6 @@ class CLEANSACMC:
         )
         self.logger.record("mc/reward", rewards.mean().item())
         return rewards
-
 
     def train(self):
         self._n_updates += 1
@@ -353,17 +382,29 @@ class CLEANSACMC:
 
         # train critic
         with torch.no_grad():
-            next_state_actions, next_state_log_pi = self.actor.get_action(next_observations)
-            crit_next_targets = self.critic_target(next_observations, next_state_actions)
+            next_state_actions, next_state_log_pi = self.actor.get_action(
+                next_observations
+            )
+            crit_next_targets = self.critic_target(
+                next_observations, next_state_actions
+            )
             min_crit_next_target = torch.min(crit_next_targets, dim=0).values
             min_crit_next_target -= ent_coef * next_state_log_pi
 
-            rewards = self.calc_reward(observations, next_state_actions, replay_data.rewards)
-            next_q_value = rewards.flatten() + \
-                           (1 - replay_data.dones.flatten()) * self.gamma * min_crit_next_target.flatten()
+            rewards = self.calc_reward(
+                observations, next_state_actions, replay_data.rewards
+            )
+            next_q_value = (
+                rewards.flatten()
+                + (1 - replay_data.dones.flatten())
+                * self.gamma
+                * min_crit_next_target.flatten()
+            )
 
         critic_a_values = self.critic(observations, replay_data.actions)
-        crit_loss = torch.stack([F.mse_loss(_a_v, next_q_value.view(-1, 1)) for _a_v in critic_a_values]).sum()
+        crit_loss = torch.stack(
+            [F.mse_loss(_a_v, next_q_value.view(-1, 1)) for _a_v in critic_a_values]
+        ).sum()
         self.logger.record("train/critic_loss", crit_loss.item())
 
         self.critic_optimizer.zero_grad()
@@ -381,16 +422,20 @@ class CLEANSACMC:
         self.actor_optimizer.step()
 
         # Update target networks with polyak update
-        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+        for param, target_param in zip(
+            self.critic.parameters(), self.critic_target.parameters()
+        ):
             target_param.data.mul_(1 - self.tau)
-            torch.add(target_param.data, param.data, alpha=self.tau, out=target_param.data)
+            torch.add(
+                target_param.data, param.data, alpha=self.tau, out=target_param.data
+            )
 
     def predict(
-            self,
-            obs: Union[np.ndarray, Dict[str, np.ndarray]],
-            state: Optional[Tuple[np.ndarray, ...]] = None,
-            episode_start: Optional[np.ndarray] = None,
-            deterministic: bool = False,
+        self,
+        obs: Union[np.ndarray, Dict[str, np.ndarray]],
+        state: Optional[Tuple[np.ndarray, ...]] = None,
+        episode_start: Optional[np.ndarray] = None,
+        deterministic: bool = False,
     ) -> Tuple[np.ndarray, None]:
         """
         Get the policy action given an observation.
@@ -405,12 +450,22 @@ class CLEANSACMC:
     def save(self, path: Union[str, pathlib.Path, io.BufferedIOBase]):
         # Copy parameter list, so we don't mutate the original dict
         data = self.__dict__.copy()
-        for to_exclude in ["logger", "env", "num_timesteps", "_n_updates", "_last_obs",
-                           "replay_buffer", "actor", "critic", "critic_target"]:
+        for to_exclude in [
+            "logger",
+            "env",
+            "num_timesteps",
+            "_n_updates",
+            "_last_obs",
+            "replay_buffer",
+            "actor",
+            "critic",
+            "critic_target",
+        ]:
             del data[to_exclude]
         # save network parameters
         data["_actor"] = self.actor.state_dict()
         data["_critic"] = self.critic.state_dict()
+        data["_mc"] = self.mc_network.state_dict()
         torch.save(data, path)
 
     @classmethod
@@ -418,11 +473,12 @@ class CLEANSACMC:
         model = cls(env=env, **kwargs)
         loaded_dict = torch.load(path)
         for k in loaded_dict:
-            if k not in ["_actor", "_critic"]:
+            if k not in ["_actor", "_critic", "_mc"]:
                 model.__dict__[k] = loaded_dict[k]
         # load network states
         model.actor.load_state_dict(loaded_dict["_actor"])
-        data["_critic"] = self.critic.state_dict()
+        model.critic.load_state_dict(loaded_data["_critic"])
+        model.mc_network.load_state_dict(loaded_data["_mc"])
         return model
 
     def set_logger(self, logger: Logger) -> None:
--- src/custom_algorithms/cleansacmc/mc.py
+++ src/custom_algorithms/cleansacmc/mc.py
@@ -5,23 +5,26 @@ import torch.nn.functional as F
 from torch.distributions import Normal
 from gymnasium import spaces
 
+LOG_STD_MAX = 2
+LOG_STD_MIN = -20
+
 class MorphologicalNetworks(nn.Module):
     def __init__(self, env, cfg):
         super().__init__()
         hidden_size = cfg["hidden_size"]
         if isinstance(env.observation_space, spaces.dict.Dict):
-            self.obs_shape = np.sum([obs_space.shape for obs_space in env.observation_space.spaces.values()])
+            self.obs_shape = np.sum(
+                [obs_space.shape for obs_space in env.observation_space.spaces.values()]
+            )
         else:
             self.obs_shape = np.sum(env.observation_space.shape)
         self.action_shape = np.prod(env.action_space.shape)
 
-
-        self.state_encoder = nn.Sequential(
+        self.state_action_encoder = nn.Sequential(
             nn.Linear(self.obs_shape + self.action_shape, hidden_size),
             nn.ReLU(),
             nn.Linear(hidden_size, hidden_size),
         )
-
         self.fw_mu = nn.Linear(hidden_size, self.obs_shape)
         self.fw_std = nn.Linear(hidden_size, self.obs_shape)
 
@@ -35,10 +38,16 @@ class MorphologicalNetworks(nn.Module):
         self.w_std = nn.Linear(hidden_size, self.obs_shape)
 
     def forward(self, obs, action):
-        hx = self.state_encoder(torch.cat([obs, action], dim=-1))
-        mu, std = self.fw_mu(hx), F.softplus(self.fw_std(hx))
+        # foward model: p(w' | w, a)
+        hx = torch.cat([obs, action], dim=-1)
+        hx = self.state_action_encoder(hx)
+        hx = F.relu(hx)
+        fw_mu, fw_log_std = self.fw_mu(hx), self.fw_std(hx)
+        fw_log_std = torch.clamp(fw_log_std, LOG_STD_MIN, LOG_STD_MAX)
 
+        # action effect model: p(w' | a)
         hw = self.action_encoder(action)
-        w_mu, w_std = self.w_mu(hw), F.softplus(self.w_std(hw))
-
-        return Normal(mu, std), Normal(w_mu, w_std)
+        hw = F.relu(hw)
+        w_mu, w_log_std = self.w_mu(hw), self.w_std(hw)
+        w_log_std = torch.clamp(w_log_std, LOG_STD_MIN, LOG_STD_MAX)
+        return Normal(fw_mu, fw_log_std.exp()), Normal(w_mu, w_log_std.exp())
