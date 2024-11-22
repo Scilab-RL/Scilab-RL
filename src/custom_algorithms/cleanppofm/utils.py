fix difficulty wrong calculation

src/custom_algorithms/cleanppofm/utils.py
@@ -645,7 +645,7 @@ def get_next_position_observation_moonlander(observations: torch.Tensor, actions
         next_observation_without_input_noise[index][1::2] -= 1
 
         # check if there is an object that already is on position -1 -> removed or if it is collected in the collect task
-        while counter < (maximum_number_of_objects * 2):
+        while counter <= (maximum_number_of_objects * 2):
             if not next_observation_without_input_noise[index][counter] == 0 and \
                     next_observation_without_input_noise[index][counter + 1] == -agent_size:
                 next_observation_without_input_noise[index][counter] = 0
@@ -945,34 +945,37 @@ def calculate_difficulty(env, policy, fm_network, logger, env_name: str,
             y_position_of_agent = int(last_observation_optimal[0][1])
             collected_objects = []
             for index in range(2, len(last_observation_optimal[0]), 2):
-                if (
-                        (
-                                ((last_observation_optimal[0][index] - 1) == (x_position_of_agent - 1))
-                                or ((last_observation_optimal[0][index] - 1) == x_position_of_agent)
-                                or ((last_observation_optimal[0][index] - 1) == (x_position_of_agent + 1))
-                                or (last_observation_optimal[0][index] == (x_position_of_agent - 1))
-                                or (last_observation_optimal[0][index] == x_position_of_agent)
-                                or (last_observation_optimal[0][index] == (x_position_of_agent + 1))
-                                or ((last_observation_optimal[0][index] + 1) == (x_position_of_agent - 1))
-                                or ((last_observation_optimal[0][index] + 1) == x_position_of_agent)
-                                or ((last_observation_optimal[0][index] + 1) == (x_position_of_agent + 1))
-                        )
-                        and
-                        (
-                                ((last_observation_optimal[0][index + 1] - 1) == (y_position_of_agent - 1))
-                                or ((last_observation_optimal[0][index + 1] - 1) == y_position_of_agent)
-                                or ((last_observation_optimal[0][index + 1] - 1) == (y_position_of_agent + 1))
-                                or (last_observation_optimal[0][index + 1] == (y_position_of_agent - 1))
-                                or (last_observation_optimal[0][index + 1] == y_position_of_agent)
-                                or (last_observation_optimal[0][index + 1] == (y_position_of_agent + 1))
-                                or ((last_observation_optimal[0][index + 1] + 1) == (y_position_of_agent - 1))
-                                or ((last_observation_optimal[0][index + 1] + 1) == y_position_of_agent)
-                                or ((last_observation_optimal[0][index + 1] + 1) == (y_position_of_agent + 1))
-                        )
-                ):
-                    collected_objects.append(
-                        {'x': int(last_observation_optimal[0][index]), 'y': int(last_observation_optimal[0][index + 1]),
-                         'size': agent_size})
+                if not last_observation_optimal[0][index] == 0 and last_observation_optimal[0][index + 1] == 0:
+
+                    if (
+                            (
+                                    ((last_observation_optimal[0][index] - 1) == (x_position_of_agent - 1))
+                                    or ((last_observation_optimal[0][index] - 1) == x_position_of_agent)
+                                    or ((last_observation_optimal[0][index] - 1) == (x_position_of_agent + 1))
+                                    or (last_observation_optimal[0][index] == (x_position_of_agent - 1))
+                                    or (last_observation_optimal[0][index] == x_position_of_agent)
+                                    or (last_observation_optimal[0][index] == (x_position_of_agent + 1))
+                                    or ((last_observation_optimal[0][index] + 1) == (x_position_of_agent - 1))
+                                    or ((last_observation_optimal[0][index] + 1) == x_position_of_agent)
+                                    or ((last_observation_optimal[0][index] + 1) == (x_position_of_agent + 1))
+                            )
+                            and
+                            (
+                                    ((last_observation_optimal[0][index + 1] - 1) == (y_position_of_agent - 1))
+                                    or ((last_observation_optimal[0][index + 1] - 1) == y_position_of_agent)
+                                    or ((last_observation_optimal[0][index + 1] - 1) == (y_position_of_agent + 1))
+                                    or (last_observation_optimal[0][index + 1] == (y_position_of_agent - 1))
+                                    or (last_observation_optimal[0][index + 1] == y_position_of_agent)
+                                    or (last_observation_optimal[0][index + 1] == (y_position_of_agent + 1))
+                                    or ((last_observation_optimal[0][index + 1] + 1) == (y_position_of_agent - 1))
+                                    or ((last_observation_optimal[0][index + 1] + 1) == y_position_of_agent)
+                                    or ((last_observation_optimal[0][index + 1] + 1) == (y_position_of_agent + 1))
+                            )
+                    ):
+                        collected_objects.append(
+                            {'x': int(last_observation_optimal[0][index]),
+                             'y': int(last_observation_optimal[0][index + 1]),
+                             'size': agent_size})
             # state for env
             last_observation_state_optimal = np.expand_dims(
                 get_observation_of_position_and_object_positions(agent_and_object_positions=last_observation_optimal,

src/custom_envs/moonlander/meta_env_pretrained.py
@@ -207,6 +207,8 @@ class MetaEnvPretrained(gym.Env):
 
         self.SoC_collect = np.array([1.0])
         self.SoC_dodge = np.array([1.0])
+        self.dodge_difficulty = -1
+        self.collect_difficulty = -1
 
         # add SoC_dodge, SoC_collect, reward_dodge, reward_collect, task_action, meta_action
         # self.state = np.append(self.state, [self.SoC_dodge, self.SoC_collect, 0, 0, 0, 0])
@@ -441,6 +443,7 @@ class MetaEnvPretrained(gym.Env):
                 else:
                     reward_dodge = active_normalized_reward
                 self.SoC_dodge = active_SoC
+                self.dodge_difficulty = active_difficulty
                 self.state_of_collect_asteroids = belief_state
                 info_collect = inactive_info
                 if self.with_SoC_in_reward:
@@ -448,6 +451,7 @@ class MetaEnvPretrained(gym.Env):
                 else:
                     reward_collect = inactive_summed_up_rewards
                 self.SoC_collect = inactive_SoC
+                self.collect_difficulty = -1
                 # for debugging
                 last_dodge_position = int(active_agent_and_object_positions_tensor[0][0])
                 last_collect_position = int(inactive_agent_and_object_positions_tensor[0][0])
@@ -483,6 +487,7 @@ class MetaEnvPretrained(gym.Env):
                 else:
                     reward_dodge = inactive_summed_up_rewards
                 self.SoC_dodge = inactive_SoC
+                self.dodge_difficulty = -1
                 self.state_of_collect_asteroids = new_state
                 info_collect = active_info
                 if self.with_SoC_in_reward:
@@ -490,6 +495,7 @@ class MetaEnvPretrained(gym.Env):
                 else:
                     reward_collect = active_normalized_reward
                 self.SoC_collect = active_SoC
+                self.collect_difficulty = active_difficulty
                 # for debugging
                 last_dodge_position = int(inactive_agent_and_object_positions_tensor[0][0])
                 last_collect_position = int(active_agent_and_object_positions_tensor[0][0])
@@ -559,7 +565,8 @@ class MetaEnvPretrained(gym.Env):
                 "predicted_dodge_next_position": predicted_next_dodge_position,
                 "predicted_collect_next_position": predicted_next_collect_position,
                 "prediction_error": active_prediction_error, "difficulty": active_difficulty,
-                "SoC_dodge": self.SoC_dodge, "SoC_collect": self.SoC_collect}
+                "SoC_dodge": self.SoC_dodge, "SoC_collect": self.SoC_collect,
+                "dodge_difficulty": self.dodge_difficulty, "collect_difficulty": self.collect_difficulty}
 
         if not self.reward_good_switch_decision:
             meta_reward = reward_dodge + reward_collect - task_switch_costs
@@ -654,6 +661,8 @@ class MetaEnvPretrained(gym.Env):
 
         self.SoC_collect = np.array([1.0])
         self.SoC_dodge = np.array([1.0])
+        self.dodge_difficulty = -1
+        self.collect_difficulty = -1
 
         # add SoC_dodge, SoC_collect, reward_dodge, reward_collect, task_action, meta_action
         # self.state = np.append(self.state, [self.SoC_dodge, self.SoC_collect, 0, 0, 0, 0])