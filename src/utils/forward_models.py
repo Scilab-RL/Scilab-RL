fe73af0efe3343d04e837ff2f08d628590823b2f
Leon Kruse <leon.kruse@tuhh.de>
2025-02-23 10:54:36 +0100

    #187 refactoring

--- src/utils/forward_models.py
+++ src/utils/forward_models.py
@@ -14,21 +14,31 @@ LOG_STD_MIN = -5
 #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device('cpu')
 
+ACTIVATION_FUNCTIONS = {
+    "relu": nn.ReLU,
+    "logistic": nn.Sigmoid,
+    "tanh": nn.Tanh
+}
+
+LOSS_FUNCTIONS = {
+    "l2": "l2_loss_delta",
+    "nll": "nll_loss_delta"
+}
+
 class ProbabilisticForwardNet(nn.Module):
     def __init__(self, config, env):
         super().__init__()
         self.hidden_size = config['hidden_size']
         self.n_hidden_layers = config['n_hidden_layers']  # Number of hidden layers
+        self.predict_reward = bool(config['predict_reward'])
+        self.cfg = config
 
-        # Activation function setup
-        if config['activation_func'] == 'relu':
-            self.activation_func = nn.ReLU
-        elif config['activation_func'] == 'logistic':
-            self.activation_func = nn.Sigmoid
-        elif config['activation_func'] == 'tanh':
-            self.activation_func = nn.Tanh
-        else:
-            assert False, "Error, activation function not valid"
+        self.activation_func = ACTIVATION_FUNCTIONS.get(config['activation_func'])
+        assert self.activation_func, "Error: Invalid activation function"
+
+        loss_func_name = LOSS_FUNCTIONS.get(config['loss_func'])
+        assert loss_func_name, "Error: Invalid loss function"
+        self.loss_func = getattr(self, loss_func_name)
 
         if isinstance(env.observation_space, spaces.Dict):
             self.obs_shape = env.observation_space.spaces['observation'].shape[0]
@@ -42,18 +52,6 @@ class ProbabilisticForwardNet(nn.Module):
 
         self.input_shape = self.obs_shape + self.action_shape
 
-        # Loss function setup
-        if config['loss_func'] == 'l2':
-            self.loss_func = self.l2_loss_delta
-        elif config['loss_func'] == 'nll':
-            self.loss_func = self.nll_loss_delta
-        else:
-            assert False, "Error: loss function invalid"
-
-        self.predict_reward = bool(config['predict_reward'])
-
-        self.cfg = config
-
     def build_hidden_layers(self, input_size, output_size):
         layers = [nn.Linear(input_size, self.hidden_size), self.activation_func()]
         for _ in range(self.n_hidden_layers - 1):  # Add additional hidden layers
@@ -64,12 +62,10 @@ class ProbabilisticForwardNet(nn.Module):
 
     def l2_loss_delta(self, obs, action, next_obs, reward):
         predictions = self(obs, action)
-        next_obs_prediction_dist = predictions["next_state"]
-        next_obs_prediction = next_obs_prediction_dist.loc
+        next_obs_prediction = predictions["next_state"].loc
         loss = (next_obs_prediction - (next_obs - obs)) ** 2
         if "reward" in predictions.keys():
-            reward_prediction = predictions["reward"]
-            loss = loss + (reward_prediction - reward) ** 2
+            loss = loss + (predictions["reward"] - reward) ** 2
         return loss
 
     def nll_loss_delta(self, obs, action, next_obs):
@@ -99,19 +95,16 @@ class ProbabilisticForwardNet(nn.Module):
     def train(self, optimizer, dataloader):
         for obs, action, next_obs, reward in dataloader:
             obs, action, next_obs, reward = obs.to(device), action.to(device), next_obs.to(device), reward.to(device)
-            loss = self.loss_func(obs, action, next_obs, reward)
             optimizer.zero_grad()
+            loss = self.loss_func(obs, action, next_obs, reward)
             loss.mean().backward()
             optimizer.step()
 
     def get_average_loss(self, dataloader):
-        losses = []
-        for obs, action, next_obs, reward in dataloader:
-            losses.append(self.loss_func(obs, action, next_obs, reward).mean().detach().item())
+        losses = [self.loss_func(obs, action, next_obs,
+                                 reward).mean().detach().item()
+                  for obs, action, next_obs, reward in dataloader]
         return np.mean(losses)
-
-    def collect_data(self, training_data:Fwd_Data, last_obs, action, new_obs, reward):
-        training_data.collect_data(last_obs, action, new_obs, reward)
 
     def save_model(self, model_name):
         torch.save(self, os.path.join(self.cfg['model_save_path'], f'{model_name}.pt'))
@@ -122,11 +115,10 @@ class ProbabilisticForwardNet(nn.Module):
 class ForwardNetEnsemble(nn.Module):
     def __init__(self, config, env, fw_class):
         super().__init__()
-        self.ensemble_size = config['ensemble_size']
         self.ensemble = nn.ModuleList(
             [
                 fw_class(config, env)
-                for _ in range(self.ensemble_size)
+                for _ in range(config['ensemble_size'])
             ]
         )
         self.training_data = Fwd_Data()
@@ -139,22 +131,23 @@ class ForwardNetEnsemble(nn.Module):
         return torch.stack([model(obs, action) for model in self.ensemble])
 
     def train(self, optimizer):
-        if self.stop_training: return
+        if self.stop_training:
+            return
         dataloader = self.training_data.get_dataloader()
         for i, model in enumerate(self.ensemble):
             model.train(optimizer[i], dataloader)
 
     def get_average_loss(self):
-        losses = []
-        for model in self.ensemble:
-            losses.append(model.get_average_loss(self.test_data.get_dataloader()))
+        losses = [model.get_average_loss(self.test_data.get_dataloader()) for model in self.ensemble]
         mean_loss = np.mean(losses)
-        if mean_loss > self.prev_loss: self.stop_training = True
+        if mean_loss > self.prev_loss:
+            self.stop_training = True
         self.prev_loss = mean_loss
         return mean_loss
 
     def collect_data(self, last_obs, action, new_obs, reward):
-        if self.stop_training: return
+        if self.stop_training:
+            return
         if np.random.rand() < 0.2:
             self.test_data.collect_data(last_obs, action, new_obs, reward)
         else:
@@ -165,14 +158,17 @@ class ForwardNetEnsemble(nn.Module):
         try:
             file_path = os.path.expanduser(self.config["model_save_path"])
             loaded_data = torch.load(file_path)
-        except:
+        except FileNotFoundError:
             return
+
         observations = loaded_data['observation']
         actions = loaded_data['action']
         next_observations = loaded_data['next_observation']
         rewards = loaded_data['reward']
+
         for i in range(len(observations)):
-            if self.stop_training: break
+            if self.stop_training:
+                break
             self.collect_data(observations[i], actions[i], next_observations[i], rewards[i])
             if i % self.config['train_every_n_data'] == 0:
                 self.train(optimizer)
@@ -198,21 +194,25 @@ class ProbabilisticForwardMLENetwork(ProbabilisticForwardNet):
 class DeterministicForwardNetwork(ProbabilisticForwardNet):
     def __init__(self, config, env):
         super().__init__(config, env)
+
         # Building the state-action model dynamically based on n_hidden_layers
         self.state_action_encoder = self.build_hidden_layers(self.input_shape, self.hidden_size)
         self.state_action_model = self.build_hidden_layers(self.hidden_size, self.obs_shape)
+
         if self.predict_reward == True:
             self.reward = self.build_hidden_layers(self.hidden_size, 1)
 
     def forward(self, obs, action):
         assert (self.obs_shape == obs.shape[-1])
         assert (self.action_shape == action.shape[-1])
+
         hx = torch.cat([obs, action], dim=-1)
         hx = self.state_action_encoder(hx)
         next_state = self.state_action_model(hx)
+
         next_state_dist = Normal(next_state, torch.zeros_like(next_state)+10**(-10))
+
         predictions = {"next_state": next_state_dist}
         if self.predict_reward:
-            reward = self.reward(hx).squeeze(-1)
-            predictions.update({"reward": reward})
+            predictions["reward"] = self.reward(hx).squeeze(-1)
         return predictions
