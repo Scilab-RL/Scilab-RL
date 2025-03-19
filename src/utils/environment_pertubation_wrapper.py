72351956710e84c730b5b90e8c45471268b4157f
Leon Kruse <leon.kruse@tuhh.de>
2025-03-19 09:56:26 +0100

    #189 further implementation of environment_pertubation_wrapper.py

--- src/utils/environment_pertubation_wrapper.py
+++ src/utils/environment_pertubation_wrapper.py
@@ -1,7 +1,7 @@
 from typing import Any, Callable, SupportsFloat
 
 import gymnasium as gym
-from gymnasium.spaces import Space
+from gymnasium.spaces import Space, Box
 from gymnasium.core import ActType, ObsType, WrapperActType
 
 """
@@ -107,3 +107,60 @@ class SleepingTransformAction(
                 raise NotImplementedError()
         else:
             raise NotImplementedError()
+
+class InvertAction(
+    SleepingTransformAction[ObsType, WrapperActType, ActType], gym.utils.RecordConstructorArgs
+):
+    """Inverts one or all dimensions of ``action`` which is passed to ``step``.
+
+    Example:
+        >>> import gymnasium as gym
+        >>> import numpy as np
+        >>> from aerl import InvertAction
+        >>> env = gym.make("Hopper-v4", disable_env_checker=True)
+        >>> env = InvertAction(env, dim=0, toggle_at_step=0)
+        >>> _ = env.reset(seed=42)
+        >>> _ = env.step(np.array([0.5, 0.5, 0.5], dtype=np.float32))
+        ... # Executes the action np.array([-0.5, 0.5, 0.5]) in the base environment
+    """
+
+    def __init__(
+        self,
+        env: gym.Env[ObsType, ActType],
+        dim: int | None = None,
+        toggle_at_step: int | list[int] | float | None = None,
+        toggle_at_episode: int | list[int] | float | None = None
+    ):
+        """A wrapper for inverting one or all dimensions of the continuous action.
+
+        Args:
+            env: The environment to wrap.
+            dim: Action dimension that is transformed. None corresponds to all dimensions.
+            toggle_at_step: Decides whether to apply transformation. If type is int, it's considered
+            True for all steps ongoing. If type is list[int], that represents a list of steps at
+            which it's alternately toggled True/False. If type is float, it's considered True for
+            that frequency of steps.
+            toggle_at_episode: Decides whether to apply transformation. If type is int, it's
+            considered True for all episodes ongoing. If type is list[int], that represents a list
+            of episodes at which it's alternately toggled True/False. If type is float, it's
+            considered True for that frequency of episodes.
+        """
+        assert isinstance(env.action_space, Box)
+
+        def _action_func(action):
+            _action = action.copy()
+            if dim is not None:
+                _action[dim] *= -1
+            else:
+                _action *= -1
+            return _action
+
+        gym.utils.RecordConstructorArgs.__init__(self)
+        SleepingTransformAction.__init__(
+            self,
+            env=env,
+            func=_action_func,
+            action_space=None,
+            toggle_at_step=toggle_at_step,
+            toggle_at_episode=toggle_at_episode
+        )
